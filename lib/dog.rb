d0bf767554cd10fd022b95ac4e2f1307e94b2c9e
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-07 19:26:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,11 @@
 class Dog
   attr_reader :name
-  attr_accessor :owner
+  attr_accessor :owner, :mood
   @@dogs = []
   def initialize(name, owner)
     @name = name
     @owner = owner
+    @mood = "nervous"
     @@dogs << self
   end
 end
